d7b122d7a02ae1ce980d7e3b54bf41b446979593
Jacko <jacko.tinoco@tecsup.edu.pe>
2023-11-22 16:29:19 -0500

    first commit

--- index.js
+++ index.js
@@ -7,14 +7,7 @@ const port = config.port;
 const connectToDatabase = require('./src/database/dbController');
 
 connectToDatabase()
-  .then(() => {
-    // Aquí puedes comenzar a trabajar con la base de datos
-    console.log('Haciendo operaciones en la base de datos...');
-  })
-  .catch((err) => {
-    console.error('Error al conectar a la base de datos:', err);
-  });
-  
+
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
 });
